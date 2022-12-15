f4c640ed01227730de394f6083e462ebaaf46c6f
Matt Clemens <mclemens68@hotmail.com>
2022-12-15 15:58:37 -0600

    more 22.3.3-2 vensim changes

--- ta-demo-rdp-containment.sh
+++ ta-demo-rdp-containment.sh
@@ -1,2 +1,2 @@
-workloader containment-switch 3389 TCP -s 2022-11-01 -e 2022-11-30 --update-pce --no-prompt
+workloader containment-switch 3389 TCP -s 2022-12-01 -e 2022-12-14 --update-pce --no-prompt
 ./ta-demo-edit-eb.sh
