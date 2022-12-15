b595ac92f614b861afa11074650d9c5504f80edd
Matt Clemens <mclemens68@hotmail.com>
2022-12-15 15:54:29 -0600

    Updated for 22.3.3-2 vensim lab

---
+++ 22.3.3-2-non-vensim/ta-demo-init.sh
@@ -0,0 +1,3 @@
+workloader wkld-import workloader-wkld-import-ta-demo.csv --umwl --update-pce --no-prompt
+workloader ruleset-import workloader-ruleset-import-ta-demo.csv --update-pce --no-prompt --provision
+workloader rule-import workloader-rule-import-ta-demo.csv --update-pce --no-prompt --provision
