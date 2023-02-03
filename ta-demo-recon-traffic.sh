5895960034950841b513bb1c60c8356e2298f1c1
Matt Clemens <mclemens68@hotmail.com>
2023-02-03 12:53:26 -0600

    Fix increase-ven-rate API change

--- ta-demo-recon-traffic.sh
+++ ta-demo-recon-traffic.sh
@@ -1,2 +1,3 @@
 workloader flow-import workloader-recon-traffic-ta-demo-rdp.csv
-workloader increase-ven-rate --update-pce --no-prompt
+workloader increase-ven-rate -a POS --update-pce --no-prompt
+workloader increase-ven-rate -a Finance --update-pce --no-prompt
